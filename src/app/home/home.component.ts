bff8bf2f6c4f7d6fe55a3b379677f6830eb7b114
Karol Góralczyk <kgoralczyk@gmail.com>
2023-06-30 14:05:48 +0200

    Add tooltip changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Course} from "../model/course";
 import {Observable} from "rxjs";
 import {CoursesService} from "../services/courses.service";
-import {map} from "rxjs/operators";
+import {map, shareReplay, tap} from "rxjs/operators";
 
 @Component({
     selector: 'home',
@@ -15,22 +15,43 @@ export class HomeComponent implements OnInit {
 
     advancedCourses$: Observable<Course[]>;
 
+    tooltipDisable: boolean=false;
+
     constructor(private coursesService: CoursesService) {
 
     }
 
     ngOnInit() {
 
-        const courses$ = this.coursesService.findAllCourses();
+        const courses$ = this.coursesService.findAllCourses().pipe(
+            map(courses=>courses
+                .sort(this.sortCoursesById)),
+            //tap(courses=>console.log(courses)),
+            shareReplay()
+        );
 
         this.beginnerCourses$ = courses$.pipe(
-          map(courses => courses.filter(course => course.category === 'BEGINNER') )
+          map(courses => courses
+             .filter(course => course.category === 'BEGINNER'))//,
+            // tap(courses=>console.log(courses))
+          
         );
 
         this.advancedCourses$ = courses$.pipe(
-            map(courses => courses.filter(course => course.category === 'ADVANCED') )
+            map(courses => courses
+                .filter(course => course.category === 'ADVANCED'))//,
+                //tap(courses=>console.log(courses))
         );
 
+    }
+
+    getTooltipDisable(): boolean {
+        console.log(this.tooltipDisable);
+        return this.tooltipDisable;
+    }
+
+    sortCoursesById(a: Course,b: Course) {
+        return a.id>=b.id ? 1 : -1;
     }
 
 }
